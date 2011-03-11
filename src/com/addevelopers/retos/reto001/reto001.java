ab84c3ba515d5f5d3b6aa3823c7d0e32a32a492d
isaac <repson0@gmailcom>
2011-03-11 01:45:05 +0100

    Creación de nuevo proyecto Android

---
+++ src/com/addevelopers/retos/reto001/reto001.java
@@ -0,0 +1,13 @@
+package com.addevelopers.retos.reto001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class reto001 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
