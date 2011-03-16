3f447895343374287f71b7969411efb439c1af31
isaac <repson0@gmailcom>
2011-03-16 13:55:53 +0100

    * Se crea la activity Isaac.java * Se implementa método onClick del botón isaac_button * Se llama a la activity Isaac.class al pulsar el botón isaac_button * Se modifica el archivo AndroidManifest.xml. No lanzaba la nueva activity hasta que he modificado el atributo "name" en la pestaña "Application", en el editor gráfico del archivo.
    Es necesario modificar el archivo AndroidManifest.xml, no me funcionaba hasta que he modificado

--- src/com/addevelopers/retos/reto001/reto001.java
+++ src/com/addevelopers/retos/reto001/reto001.java
@@ -1,7 +1,14 @@
 package com.addevelopers.retos.reto001;
 
+import com.anddevelopers.retos.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
 
 public class reto001 extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +16,17 @@ public class reto001 extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        final String TAG = "reto001";
+                
+        final Button button_carlos = (Button) findViewById(R.id.carlos_button);
+        final Button button_isaac = (Button) findViewById(R.id.isaac_button);
+        
+        button_isaac.setOnClickListener(new Button.OnClickListener(){
+        	public void onClick(final View v){
+        		Log.i(TAG, "button_isaac: onClick invoked");
+        		Intent intent = new Intent(reto001.this, Isaac.class);
+        		reto001.this.startActivity(intent);
+        	}
+        });
     }
 }
