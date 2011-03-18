2a6ec32d63c1984404d7f7281950012296ada81a
isaac <repson0@gmailcom>
2011-03-18 01:51:21 +0100

    Pruebas con Toast Notifications

--- gen/com/anddevelopers/retos/reto001/R.java
+++ gen/com/anddevelopers/retos/reto001/R.java
@@ -18,12 +18,15 @@ public final class R {
     }
     public static final class id {
         public static final int about_content=0x7f070000;
-        public static final int carlos_button=0x7f070004;
-        public static final int help=0x7f070005;
-        public static final int isaac_button=0x7f070003;
+        public static final int carlos_button=0x7f070007;
+        public static final int help=0x7f070008;
+        public static final int isaac_button=0x7f070006;
         public static final int isaac_main_button1=0x7f070001;
         public static final int isaac_main_button2=0x7f070002;
-        public static final int quit=0x7f070006;
+        public static final int isaac_main_button3=0x7f070003;
+        public static final int isaac_main_button4=0x7f070004;
+        public static final int isaac_main_button5=0x7f070005;
+        public static final int quit=0x7f070009;
     }
     public static final class layout {
         public static final int carlos=0x7f030000;
--- src/com/anddevelopers/retos/reto001/Isaac.java
+++ src/com/anddevelopers/retos/reto001/Isaac.java
@@ -3,25 +3,41 @@ package com.anddevelopers.retos.reto001;
 import com.anddevelopers.retos.reto001.R;
 import android.view.MenuInflater;
 import android.view.Menu;
+import android.view.View;
 import android.util.Log;
 import android.view.MenuItem;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Context;
+
 
 public class Isaac extends Activity{ 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.isaac);
+		
+		 final Button isaac_main_button1 = (Button)findViewById(R.id.isaac_main_button1);
+	        
+	     isaac_main_button1.setOnClickListener(new Button.OnClickListener(){
+	     public void onClick(final View v){
+	    	 Context context = getApplicationContext();
+	    	 CharSequence text = "Hello toast notification!";
+	    	 int duration = Toast.LENGTH_SHORT;
+	    	 
+	    	 Toast toast = Toast.makeText(context, text, duration);
+	    	 toast.show();
+	     	}
+	     });
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-//	    MenuInflater inflater = getMenuInflater();
-//	    inflater.inflate(R.menu.isaac_main, menu);
-		menu.add(0, 1, 0, "Help");
-		menu.add(0, 2, 1, "Quit");
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.isaac_main, menu);
 	    return true;
 	}
 	
@@ -29,14 +45,15 @@ public class Isaac extends Activity{
 	public boolean onOptionsItemSelected(MenuItem it){
 		final String TAG = "Isaac";
 		switch (it.getItemId()){
-		case 1:
+		case R.id.help:
 			Log.i(TAG, "case 1: Help");
-//    		Intent intent = new Intent(Isaac.this, Isaac_help.class);
-//    		startActivity(intent);
+			Toast.makeText(getApplicationContext(), 
+					"Help Option is Selected!", Toast.LENGTH_SHORT).show();
 			break;
-		case 2:
+		case R.id.quit:
 			Log.i(TAG, "case 2: Quit");
-			System.exit(1);
+			Toast.makeText(getApplicationContext(), 
+					"Quit Option is Selected!", Toast.LENGTH_SHORT).show();
 			break;
 		default:
 			Log.i(TAG, "case default");
